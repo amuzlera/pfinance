b272753183d8e9fb0494f17b13ff5328c158898d
amuzlera <71038398+amuzlera@users.noreply.github.com>
2025-02-22 01:28:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,61 @@ import os
 from parsers.movimientos_mp_parser import parse_transactions_from_mp
 from parsers.movimientos_santander_parser import parse_movimientos_santander
 from parsers.visa_resumen_parser import create_df_from_pdf, parse_consumo
-import requests
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
-import json
 import calendar
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
 import hashlib
+from streamlit_date_picker import date_range_picker, date_picker, PickerType
 
 DB_NAME = "movimientos.db"
 
-if os.path.exists("tags.json"): 
-    with open("tags.json", "r") as f:
-        TAGS_NAMES_MAP = json.load(f)
-else:
-    TAGS_NAMES_MAP = {}
+def get_data_from_db(table_name):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            tag_name TEXT,
+            keywords TEXT,
+            id TEXT PRIMARY KEY
+        )
+    ''')
+    cursor.execute(f'SELECT tag_name, keywords FROM {table_name}')
+    rows = cursor.fetchall()
+    conn.close()
+    data_map = {}
+    for row in rows:
+        tag_name, keywords = row
+        data_map[tag_name] = keywords.split(',')
+    return data_map
 
-if os.path.exists("alias.json"):
-    with open("alias.json", "r") as f:
-        ALIAS_NAMES_MAP = json.load(f)
-else:
-    ALIAS_NAMES_MAP = {}
+def save_data_to_db(data_map, table_name):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            tag_name TEXT,
+            keywords TEXT,
+            id TEXT PRIMARY KEY
+        )
+    ''')
+    for id, tag_name_keywords in enumerate(data_map.items()):
+        tag_name, keywords = tag_name_keywords
+        keywords_str = ','.join(keywords)
+        cursor.execute(f'''
+            INSERT OR REPLACE INTO {table_name} (tag_name, keywords, id)
+            VALUES (?, ?, ?)
+        ''', (tag_name, keywords_str, id))
+    conn.commit()
+    conn.close()
+
+
+
+TAGS_NAMES_MAP = get_data_from_db('tags')
+ALIAS_NAMES_MAP = get_data_from_db('alias')
+
+
+
 
 VALID_EXTENSIONS = (".xlsx", ".xls", ".pdf")
 
@@ -53,7 +87,6 @@ def save_to_db(df):
             categoria TEXT,
             alias TEXT,
             cuotas TEXT
-                   
         )
     ''')
 
@@ -64,7 +97,12 @@ def save_to_db(df):
     conn.close()
 
 def read_from_db():
-    if not os.path.exists(DB_NAME):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='movimientos'")
+    table_exists = cursor.fetchone()
+    conn.close()
+    if not table_exists:
         return pd.DataFrame(columns=['date', 'monto', 'id', 'nombre', 'categoria', 'alias'])
     conn = sqlite3.connect(DB_NAME)
     df = pd.read_sql_query('SELECT * FROM movimientos', conn)
@@ -76,7 +114,6 @@ def load_db():
     if 'alias' not in df.columns:
         df['alias'] = ""
 
-    print(df['monto'])
     df['date'] = pd.to_datetime(df['date'])
     df['monto'] = df['monto'].astype(float)
     df['alias'] = df['alias'].fillna('')
@@ -115,7 +152,7 @@ def load_data_from_files(uploaded_files):
     save_to_db(data)
 
 def order_df(df):
-    first_columns = ['date', 'alias', 'nombre', 'cuotas', 'monto']
+    first_columns = ['date', 'nombre', 'monto', 'cuotas', 'alias']
     remaining_columns = [col for col in df.columns if col not in first_columns]
     column_order = first_columns + remaining_columns
     df = df.reindex(columns=column_order)
@@ -128,14 +165,8 @@ def add_tags(data, tags_map, col_name, default_tag='otros'):
     for tag, keywords in tags_map.items():
         for keyword in keywords:
             data.loc[data['nombre'].str.contains(keyword, case=False), col_name] = tag
-    data[col_name] = data[col_name].fillna(default_tag)
+    data[col_name] = data[col_name].fillna(default_tag) 
 
-    return data
-
-def transform_usd_to_ars(data):
-    usd_data = requests.get("https://dolarapi.com/v1/dolares/blue").json()
-    usd_to_ars = (usd_data["compra"] + usd_data["venta"]) / 2
-    data['monto'] = data.apply(lambda row: row['monto'] * usd_to_ars if "usd" in row['nombre'].lower() else row['monto'], axis=1)
     return data
 
 def generate_id(row):
@@ -214,121 +245,35 @@ def edit_data(df):
                 save_to_db(modified_data)
                 st.rerun()
 
+def create_month_range_picker():
+    default_start, default_end = datetime.now() - timedelta(days=30), datetime.now()
+    refresh_value = timedelta(days=30)
+    refresh_buttons = [{
+                        'button_name': 'Refresh Last 1 Month',
+                        'refresh_value': refresh_value
+                    }]
+    date_range_string = date_range_picker(picker_type=PickerType.month,
+                                        start=default_start, end=default_end,
+                                        key='month_range_picker',
+                                        refresh_buttons=refresh_buttons)
+    st.session_state.start_datetime = date_range_string[0]
+    st.session_state.end_datetime = date_range_string[1]
 
+def create_custom_range_picker():
+    default_start, default_end = datetime.now() - timedelta(days=1), datetime.now()        
+    date_range_string = date_range_picker(picker_type=PickerType.date,
+                                        start=default_start, end=default_end,
+                                        key='date_range_picker')
 
-
-def create_date_selector():
-    if 'month_index' not in st.session_state:
-        st.session_state.month_index = datetime.now().month - 1
-    if 'year' not in st.session_state:
-        st.session_state.year = datetime.now().year
-
-    # Function to cycle months
-    def cycle_months(direction):
-        if direction == 'prev':
-            st.session_state.month_index = (st.session_state.month_index - 1) % 12
-            if st.session_state.month_index == 11:
-                st.session_state.year -= 1
-        elif direction == 'next':
-            st.session_state.month_index = (st.session_state.month_index + 1) % 12
-            if st.session_state.month_index == 0:
-                st.session_state.year += 1
-        st.rerun()
-
-    # Function to modify year
-    def modify_year(direction):
-        if direction == 'prev':
-            st.session_state.year -= 1
-        elif direction == 'next':
-            st.session_state.year += 1
-        st.rerun()
-
-    # Display month and year carousel
-    prev_month_button, month_label, next_month_button, prev_year_button, year_label, next_year_button = st.columns([1, 2, 1, 1, 1, 1])
-    with prev_month_button:
-        if st.button("←", key="prev_month"):
-            cycle_months('prev')
-    with month_label:
-        st.write(f"### {calendar.month_name[st.session_state.month_index + 1]}")
-    with next_month_button:
-        if st.button("→", key="next_month"):
-            cycle_months('next')
-    with prev_year_button:
-        if st.button("←", key="prev_year"):
-            modify_year('prev')
-    with year_label:
-        st.write(f"### {st.session_state.year}")
-    with next_year_button:
-        if st.button("→", key="next_year"):
-            modify_year('next')
+    st.session_state.start_datetime = date_range_string[0]
+    st.session_state.end_datetime = date_range_string[1]
 
 def filter_data_by_date(data):
-    return data[(data['date'].dt.month == st.session_state.month_index + 1) & (data['date'].dt.year == st.session_state.year)]
-
-def pfinance_app():
-    if uploaded_files := st.file_uploader("Subir archivos", type=VALID_EXTENSIONS, accept_multiple_files=True):
-        load_data_from_files(uploaded_files)
-
-    st.title("Gastos")
-    create_date_selector()
-
-    if 'selected_category' not in st.session_state:
-        st.session_state.selected_category = "todos"
-
-    data = load_db()
-    data = add_tags(data, tags_map=TAGS_NAMES_MAP, col_name='categoria')
-    data = add_tags(data, tags_map=ALIAS_NAMES_MAP, col_name='alias', default_tag="")
-    data = filter_data_by_date(data)
-
-    # Create buttons
-    columns = st.columns(len(TAGS_NAMES_MAP) + 1)
-    if columns[0].button("todos"):
-        st.session_state.selected_category = "todos"
-
-    for i, category in enumerate(TAGS_NAMES_MAP.keys(), start=1):
-        if columns[i].button(category):
-            st.session_state.selected_category = category
-
-
-    if st.session_state.selected_category == "todos":
-        grouped_data = data.groupby('categoria')['monto'].sum().reset_index()
-        grouped_data['color'] = grouped_data['categoria'].map(TAGS_COLORS_MAP)
-        filtered_data = grouped_data
-        filter_column = 'categoria'
-        total_amount = filtered_data['monto'].sum()
-        fig = px.pie(filtered_data, names=filter_column, values='monto', title='Distribución de Gastos', color='categoria', color_discrete_map=TAGS_COLORS_MAP, hole=0.4)
-        fig.add_annotation(text=f"${total_amount:,.0f}", x=0.5, y=0.5, font_size=20, showarrow=False)
-        selected_point = st.plotly_chart(fig, use_container_width=True)
-        styled_df = data.style.apply(color_rows, axis=1)
-
-    else:
-        filtered_data = data[data['categoria'] == st.session_state.selected_category]
-        unique_names = filtered_data['nombre'].unique()
-        unique_colors = px.colors.qualitative.Plotly
-        name_color_map = {name: unique_colors[i % len(unique_colors)] for i, name in enumerate(unique_names)}
-        filtered_data['label'] = filtered_data.apply(lambda row: row['alias'] if row['alias'] else row['nombre'], axis=1)
-        filter_column = 'label'
-        filtered_data['color'] = filtered_data['nombre'].map(name_color_map)
-        total_amount = filtered_data['monto'].sum()
-        fig = px.pie(filtered_data, names=filter_column, values='monto', title='Distribución de Gastos', color='nombre', color_discrete_map=name_color_map, hole=0.4)
-        fig.add_annotation(text=f"${total_amount:,.0f}", x=0.5, y=0.5, font_size=20, showarrow=False)
-        selected_point = st.plotly_chart(fig, use_container_width=True)
-        styled_df = filtered_data.style.apply(lambda row: ['background-color: {}'.format(row['color'])] * len(row), axis=1)
-
-    styled_df = styled_df.format({'monto': lambda x: f"${x:,.0f}"})
-    styled_df.data['date'] = pd.to_datetime(styled_df.data['date']).dt.date
-
-    height = len(styled_df.data) * 45
-    width = 200 + len(styled_df.data.columns) * 150
-    st.dataframe(styled_df, hide_index=True, column_config={"color": None, "id": None, "label": None, 'raw': None}, width=width)
-
-
-    edit_data(styled_df.data)
-    add_tags_form()
-    add_expense_form()
-    search_expense_panel()
-
-
+    if 'start_datetime' in st.session_state and 'end_datetime' in st.session_state:
+        start_date = st.session_state.start_datetime
+        end_date = st.session_state.end_datetime
+        return data[(data['date'] >= start_date) & (data['date'] <= end_date)]
+    return data
 
 def add_tags_form():
     st.subheader("Agregar nueva etiqueta o alias")
@@ -358,9 +303,7 @@ def add_tags_form():
             if tag_name and keywords:
                 new_keywords = [kw.strip() for kw in keywords.split(",")]
                 TAGS_NAMES_MAP[tag_name].extend(new_keywords)
-
-                with open("tags.json", "w") as f:
-                    json.dump(TAGS_NAMES_MAP, f, indent=4)
+                save_data_to_db(TAGS_NAMES_MAP, 'tags')
 
                 st.success(f"Etiqueta '{tag_name}' agregada con éxito.")
             else:
@@ -375,9 +318,7 @@ def add_tags_form():
                     ALIAS_NAMES_MAP[alias_name] = []
                 new_keywords = [kw.strip() for kw in keywords.split(",")]
                 ALIAS_NAMES_MAP[alias_name].extend(new_keywords)
-
-                with open("alias.json", "w") as f:
-                    json.dump(ALIAS_NAMES_MAP, f, indent=4)
+                save_data_to_db(ALIAS_NAMES_MAP, 'alias')
 
                 st.success(f"Alias '{alias_name}' agregado con éxito.")
             else:
@@ -386,23 +327,33 @@ def add_tags_form():
 def search_expense_panel():
     st.subheader("Buscar Gasto")
 
-    search_option = st.selectbox("Buscar por", ["ID", "Nombre"])
+    search_option = st.selectbox("Buscar por", ["ID", "Nombre", "tags", "alias"])
     search_query = st.text_input("Ingrese el valor de búsqueda")
 
     if st.button("Buscar"):
         if search_option == "ID":
-            query = f"SELECT * FROM movimientos WHERE id = '{search_query}'"
-        else:
-            query = f"SELECT * FROM movimientos WHERE nombre LIKE '%{search_query}%'"
+            table = "movimientos"
+            query = f"SELECT * FROM {table} WHERE id = '{search_query}'"
+        elif search_option == "Nombre":
+            table = "movimientos"
+            query = f"SELECT * FROM {table} WHERE nombre LIKE '%{search_query}%'"
+        elif search_option == "tags":
+            table = "tags"
+            query = f"SELECT * FROM {table}"
+        elif search_option == "alias":
+            table = "alias"
+            query = f"SELECT * FROM {table}"
+        
 
         conn = sqlite3.connect(DB_NAME)
         result_df = pd.read_sql_query(query, conn)
         conn.close()
 
-        st.session_state['search_results'] = result_df.to_dict('records')
+        st.session_state['search_results'] = result_df.to_dict('records'), table
 
     if 'search_results' in st.session_state:
-        result_df = pd.DataFrame(st.session_state['search_results'])
+        result_df_saved, table = st.session_state['search_results']
+        result_df = pd.DataFrame(result_df_saved)
         if not result_df.empty:
             st.write("Resultados de la búsqueda:")
             for index, row in result_df.iterrows():
@@ -411,21 +362,92 @@ def search_expense_panel():
                     st.write(row.to_frame().T)
                 with col2:
                     if st.button("Eliminar", key=f"delete_{row['id']}"):
-                        delete_expense(row['id'])
+                        delete_expense(row['id'], table)
                         with col1:
                             st.success(f"Gasto con ID {row['id']} eliminado.")
-                        st.session_state['search_results'] = result_df[result_df['id'] != row['id']].to_dict('records')
+                        st.session_state['search_results'] = result_df[result_df['id'] != row['id']].to_dict('records'), table
                         sleep(2)
                         st.rerun()
         else:
             st.write("No se encontraron resultados.")
 
-def delete_expense(expense_id):
+def delete_expense(expense_id, table):
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM movimientos WHERE id = ?", (expense_id,))
+    cursor.execute(f"DELETE FROM {table} WHERE id = ?", (expense_id,))
     conn.commit()
     conn.close()
+
+def create_category_buttons():
+    categories = ["todos"] + list(TAGS_NAMES_MAP.keys())
+    cols = st.columns(len(categories))
+
+    for i, category in enumerate(categories):
+        if cols[i].button(category):
+            st.session_state.selected_category = category
+
+
+
+def pfinance_app():
+    create_custom_range_picker()
+    create_month_range_picker()
+    if uploaded_files := st.file_uploader("Subir archivos", type=VALID_EXTENSIONS, accept_multiple_files=True):
+        load_data_from_files(uploaded_files)
+
+    st.title("Categorias")
+
+    if 'selected_category' not in st.session_state:
+        st.session_state.selected_category = "todos"
+
+    data = load_db()
+    data = add_tags(data, tags_map=TAGS_NAMES_MAP, col_name='categoria')
+    data = add_tags(data, tags_map=ALIAS_NAMES_MAP, col_name='alias', default_tag="")
+    data = filter_data_by_date(data)
+    create_category_buttons()
+
+
+    st.write(f'### Distribucion de gastos entre: {st.session_state.start_datetime} y {st.session_state.end_datetime}')
+    if st.session_state.selected_category == "todos":
+        grouped_data = data.groupby('categoria')['monto'].sum().reset_index()
+        grouped_data['color'] = grouped_data['categoria'].map(TAGS_COLORS_MAP)
+        filtered_data = grouped_data
+        filter_column = 'categoria'
+        total_amount = filtered_data['monto'].sum()
+        filtered_data_all_positive = filtered_data.copy()
+        filtered_data_all_positive['monto'] = filtered_data_all_positive['monto'].abs()
+        fig = px.pie(filtered_data_all_positive, names=filter_column, values='monto', color='categoria', color_discrete_map=TAGS_COLORS_MAP, hole=0.4)
+        fig.add_annotation(text=f"${total_amount:,.0f}", x=0.5, y=0.5, font_size=20, showarrow=False)
+        selected_point = st.plotly_chart(fig, use_container_width=True)
+        styled_df = data.style.apply(color_rows, axis=1)
+
+    else:
+        filtered_data = data[data['categoria'] == st.session_state.selected_category]
+        unique_names = filtered_data['nombre'].unique()
+        unique_colors = px.colors.qualitative.Plotly
+        name_color_map = {name: unique_colors[i % len(unique_colors)] for i, name in enumerate(unique_names)}
+        filtered_data['label'] = filtered_data.apply(lambda row: row['alias'] if row['alias'] else row['nombre'], axis=1)
+        filter_column = 'label'
+        filtered_data['color'] = filtered_data['nombre'].map(name_color_map)
+        total_amount = filtered_data['monto'].sum()
+        filtered_data_all_positive = filtered_data.copy()
+        filtered_data_all_positive['monto'] = filtered_data_all_positive['monto'].abs()
+        fig = px.pie(filtered_data_all_positive, names=filter_column, values='monto', color='nombre', color_discrete_map=name_color_map, hole=0.4)
+        fig.add_annotation(text=f"${total_amount:,.0f}", x=0.5, y=0.5, font_size=20, showarrow=False)
+        selected_point = st.plotly_chart(fig, use_container_width=True)
+        styled_df = filtered_data.style.apply(lambda row: ['background-color: {}'.format(row['color'])] * len(row), axis=1)
+
+    styled_df = styled_df.format({'monto': lambda x: f"${x:,.0f}"})
+    styled_df.data['date'] = pd.to_datetime(styled_df.data['date']).dt.date
+
+    height = len(styled_df.data) * 45
+    width = 200 + len(styled_df.data.columns) * 150
+    st.dataframe(styled_df, hide_index=True, column_config={"color": None, "id": None, "label": None, 'raw': None}, width=width)
+
+
+    edit_data(styled_df.data)
+    add_tags_form()
+    add_expense_form()
+    search_expense_panel()
 
 
 if __name__ == "__main__":
